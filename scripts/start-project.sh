16fe6ac519fd33866377bc3a50db3011894bf76f
Heena Sharma <heena.sharma@thoughts2binary.com>
2023-02-17 22:58:54 +0530

    Requirements and backend script

--- scripts/start-project.sh
+++ scripts/start-project.sh
@@ -4,7 +4,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
-
 start_process() {
     echo "${GREEN}Starting script!!!!!!"
 
@@ -15,22 +14,32 @@ start_process() {
     echo "${YELLOW} Cleaning the frontend folder..."
     rm -rf node_modules build 
     
-    if ! command -v yarn &> /dev/null
-    then
-        echo "${RED}yarn not found installing...."
-        npm i -g yarn
-        echo "${GREEN}yarn installed sucessfully"
-        exit
-    fi
+#    if ! command -v yarn &> /dev/null
+#    then
+#        echo "${RED}yarn not found installing...."
+#        npm i -g yarn
+#        echo "${GREEN}yarn installed sucessfully"
+#        exit
+#    fi
 
-    echo "${GREEN}Running yarn install..."
-    npx yarn install
+    echo "${GREEN}Running npm install..."
+    npx npm install
 
     echo "${GREEN}Building and relocate the static files..."
-    yarn run relocate    
+    npm run relocate
 
     echo "${GREEN}Frontend is Depolyed now processing backend..."
-
+# TODO:
+    pip install virtualenv
+    virtualenv -p python ../venv
+    cd ../backend
+    pip install -r requirements.txt
+    python manage.py migrate
+    python manage.py runserver
+# Check if venv exist, if not create venv
+# Install requirements
+# Run migrations
+# Run Server
 }
 
 start_process
