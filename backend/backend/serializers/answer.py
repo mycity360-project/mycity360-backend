3d09b8fd754165282486ec267a94d70cc89e4f43
Heena Sharma <heena.sharma@thoughts2binary.com>
2023-04-19 23:10:08 +0530

    Question in answer api

--- backend/backend/serializers/answer.py
+++ backend/backend/serializers/answer.py
@@ -8,7 +8,7 @@ class AnswerSerializer(serializers.ModelSerializer):
         return dict(
             user=dict(id=data.get("user").get("id")),
             user_ad=dict(id=data.get("user_ad").get("id")),
-            question=dict(id=data.get("question").get("id")),
+            question=dict(id=data.get("question").get("id"), question=data.get("question").get("question")),
             created_date=data.get("created_date"),
             updated_date=data.get("updated_date"),
             extra_data=data.get("extra_data"),
