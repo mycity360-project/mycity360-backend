f6210e1d6302d43023797b12cd5eaa0d32290b53
Heena Sharma <heena.sharma@thoughts2binary.com>
2023-05-06 01:02:31 +0530

    Updated image

--- backend/backend/serializers/image.py
+++ backend/backend/serializers/image.py
@@ -8,7 +8,7 @@ class ImageSerializer(serializers.ModelSerializer):
     def serialize_data(cls, data):
         return dict(
             id=data.id,
-            image=serialize_image(data.image.url if data.image else None),
+            image=data.image,
         )
 
     class Meta:
