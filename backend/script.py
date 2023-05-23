055a552e492e0c0962d3814ea8363cbe52a1d39f
Heena Sharma <heena.sharma@thoughts2binary.com>
2023-05-23 23:11:26 +0530

    Updated script

--- backend/script.py
+++ backend/script.py
@@ -38,45 +38,46 @@ def upload_to_local(
     except:
         print(traceback.format_exc())
 
+#
+# category = Category.objects.all()
+# for cat in category:
+#     if cat.icon:
+#         try:
+#             response = requests.get(cat.icon)
+#             path = upload_to_local(
+#                 response.content,
+#                 ext=f'.{cat.icon.split(".")[-1]}',
+#                 folder="category",
+#             )
+#             c = Category.objects.get(id=cat.id)
+#             c.icon = path
+#             c.save()
+#         except Exception as e:
+#             print("Category")
+#             print(e)
 
-category = Category.objects.all()
-for cat in category:
-    if cat.icon:
-        try:
-            response = requests.get(cat.icon)
-            path = upload_to_local(
-                response.content,
-                ext=f'.{cat.icon.split(".")[-1]}',
-                folder="category",
-            )
-            c = Category.objects.get(id=cat.id)
-            c.icon = path
-            c.save()
-        except Exception as e:
-            print("Category")
-            print(e)
-
-banner = Banner.objects.all()
-for cat in banner:
-    if cat.image:
-        try:
-            response = requests.get(cat.image)
-            path = upload_to_local(
-                response.content,
-                ext=f'.{cat.image.split(".")[-1]}',
-                folder="banner",
-            )
-            c = Banner.objects.get(id=cat.id)
-            c.image = path
-            c.save()
-        except Exception as e:
-            print("Banner")
-            print(e)
+# banner = Banner.objects.all()
+# for cat in banner:
+#     if cat.image:
+#         try:
+#             response = requests.get(cat.image)
+#             path = upload_to_local(
+#                 response.content,
+#                 ext=f'.{cat.image.split(".")[-1]}',
+#                 folder="banner",
+#             )
+#             c = Banner.objects.get(id=cat.id)
+#             c.image = path
+#             c.save()
+#         except Exception as e:
+#             print("Banner")
+#             print(e)
 
 service = Service.objects.all()
 for cat in service:
     if cat.icon:
         try:
+            print(cat.id)
             response = requests.get(cat.icon)
             print("=============")
             print(response)
@@ -86,6 +87,7 @@ for cat in service:
                 folder="service",
             )
             c = Service.objects.get(id=cat.id)
+            print(c.icon)
             c.icon = path
             c.save()
         except Exception as e:
